748962852d0c7fcd622daaa9c6d1bd7dc9774c4e
zhaohao <1154283293@qq.com>
2020-03-22 23:38:12 -0400

    push goenvbuild.sh file

---
+++ 04goenvbuild.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# 1.下载安装包并解压
+curl -O https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
+tar -zxvf go1.11.5.linux-amd64.tar.gz -C /usr/local/ > /dev/null 
+
+# 2. 设置环境变量
+echo 'export GOROOT=/usr/local/go/' >> /etc/profile 
+echo 'export PATH=$PATH:$GOROOT/bin' >> /etc/profile
+
+# 3. 环境变量生效并测试清理
+source /etc/profile
+go version
+if [ $? -eq 0 ];then
+    rm -rf go1.11.5.linux-amd64.tar.gz
+    echo "安装成功！"
+else 
+    echo "安装失败"
+fi
