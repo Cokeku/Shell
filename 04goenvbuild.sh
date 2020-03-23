c50a6ecfaacc6d62904b72ae9b44f870d58a53dd
zhaohao <1154283293@qq.com>
2020-03-23 07:06:24 -0400

    update go version

--- 04goenvbuild.sh
+++ 04goenvbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 1.下载安装包并解压
-curl -O https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
-tar -zxvf go1.11.5.linux-amd64.tar.gz -C /usr/local/ > /dev/null 
+curl -O https://dl.google.com/go/go1.13.9.linux-amd64.tar.gz
+tar -zxvf go1.13.9.linux-amd64.tar.gz -C /usr/local/ > /dev/null 
 
 # 2. 设置环境变量
 echo 'export GOROOT=/usr/local/go/' >> /etc/profile 
@@ -11,7 +11,7 @@ echo 'export PATH=$PATH:$GOROOT/bin' >> /etc/profile
 source /etc/profile
 go version
 if [ $? -eq 0 ];then
-    rm -rf go1.11.5.linux-amd64.tar.gz
+    rm -rf go1.13.9.linux-amd64.tar.gz
     echo "安装成功！"
 else 
     echo "安装失败"
