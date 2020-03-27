417ef84b190b783907866b0aee223257e2a54b70
zhaohao <1154283293@qq.com>
2020-03-27 08:21:43 -0400

    push dockerinstall.sh

---
+++ 05getdocker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# step 1: 安装必要的一些系统工具
+yum install -y yum-utils device-mapper-persistent-data lvm2
+# Step 2: 添加软件源信息
+yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+# Step 3: 安装Docker-CE 18.06.3.ce-3.el7
+yum makecache fast
+yum -y install docker-ce-18.06.3.ce-3.el7
+# Step 4: 开启Docker服务
+systemctl start docker
+# Step 5: 通过启动hello-world验证
+docker run hello-world
+# Step 6： 配置镜像加速器
+mkdir -p /etc/docker
+tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://rjy7eqvs.mirror.aliyuncs.com"]
+}
+EOF
+systemctl daemon-reload
+systemctl restart docker
